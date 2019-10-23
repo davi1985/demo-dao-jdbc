1cf0e90cb522e493f9a0a8826fb701e20a95f04a
Davi Silva <davisilvaphoto@gmail.com>
2019-10-23 13:39:13 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,28 +2,14 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-
-        Seller seller = new Seller(
-                21,
-                "Davi Silva",
-                "dssilvap@hotmail.com",
-                new Date(),
-                3000.0,
-                obj
-        );
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println(obj);
-
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
