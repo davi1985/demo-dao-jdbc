ad68628cd7e18d9f5777dde0fd05748cd73b4dcc
Davi Silva <davisilvaphoto@gmail.com>
2019-10-23 13:15:06 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,6 +10,7 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
+
         Seller seller = new Seller(
                 21,
                 "Davi Silva",
@@ -16,7 +19,12 @@ public class Program {
                 3000.0,
                 obj
         );
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(obj);
+
+
         System.out.println(seller);
     }
 }
