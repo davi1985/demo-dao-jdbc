b68a9882bc6e1f6042e058d7dad25789b894c243
Davi Silva <davisilvaphoto@gmail.com>
2019-10-27 13:21:19 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,10 +8,12 @@ import model.entities.Seller;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== Test 1: seller findbyid ===");
@@ -45,5 +47,13 @@ public class Program {
         seller.setName("Davi Silva");
         sellerDao.update(seller);
         System.out.println("Update completed!");
+
+        System.out.println("\n=== Test 6: seller delete ===");
+        System.out.println("Enter id for delete teste: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed!");
+
+        sc.close();
     }
 }
